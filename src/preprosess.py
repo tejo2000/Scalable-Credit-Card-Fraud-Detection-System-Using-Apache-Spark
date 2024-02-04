7f51b391b70a376bb7b343bfb156d19d6cc1b45a
tejosumanth <pavuluritejo@gmail.com>
2024-02-04 21:27:00 +0000

    Add validation and error handling to preprosess.py

--- src/preprosess.py
+++ src/preprosess.py
@@ -2,3 +2,7 @@ import os
 import sys
 import logging
 from dotenv import load_dotenv
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.feature import StandardScaler
+
+from utils import get_file_from_S3, get_spark
