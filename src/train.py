e772552c811d0f887476a3c6ce84bb7473ebe58f
tejosumanth <pavuluritejo@gmail.com>
2024-02-07 21:18:00 +0000

    Extend train.py with additional functionality

--- src/train.py
+++ src/train.py
@@ -2,3 +2,7 @@
 import os
 import sys
 import shutil
+import logging
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from dotenv import load_dotenv
