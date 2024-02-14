49a27c5d43c9a1f442f06d400bdcb20a5b0f8943
tejosumanth <pavuluritejo@gmail.com>
2024-02-14 19:30:00 +0000

    Add more functions to inference.py

--- src/inference.py
+++ src/inference.py
@@ -6,3 +6,7 @@ import uuid
 import pandas as pd
 from dotenv import load_dotenv
 from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.feature import StandardScaler
+from pyspark.ml.classification import RandomForestClassificationModel
+
+from constants import BUCKET_NAME, TEMP_MODEL_PATH, PREDICTION_TABLE
