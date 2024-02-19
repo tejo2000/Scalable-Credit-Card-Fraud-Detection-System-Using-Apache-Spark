75f72e8b0777a83716744ef6f750f757f913cf4d
tejosumanth <pavuluritejo@gmail.com>
2024-02-19 19:24:00 +0000

    Flesh out preprosess.py implementation

--- src/preprosess.py
+++ src/preprosess.py
@@ -6,3 +6,7 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.feature import StandardScaler
 
 from utils import get_file_from_S3, get_spark
+
+load_dotenv()
+
+hdfs_file_path = os.environ['HDFS_FILE_PATH']
