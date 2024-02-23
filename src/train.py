bf3f50ccc6e930221d1fbdf1a3a4b7729a0fb960
tejosumanth <pavuluritejo@gmail.com>
2024-02-23 18:36:00 +0000

    Add additional fields and logic to train.py

--- src/train.py
+++ src/train.py
@@ -6,3 +6,7 @@ import logging
 from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from dotenv import load_dotenv
+
+from constants import BUCKET_NAME, TEMP_MODEL_PATH
+from utils import get_spark, upload
+
