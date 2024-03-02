cb16bb3b4324e7db990a661769da09ebf4e3b182
tejosumanth <pavuluritejo@gmail.com>
2024-03-02 18:09:00 +0000

    Add additional fields and logic to inference.py

--- src/inference.py
+++ src/inference.py
@@ -10,3 +10,7 @@ from pyspark.ml.feature import StandardScaler
 from pyspark.ml.classification import RandomForestClassificationModel
 
 from constants import BUCKET_NAME, TEMP_MODEL_PATH, PREDICTION_TABLE
+from utils import get_file_from_S3, get_spark, download, store_prediction
+
+load_dotenv()
+
