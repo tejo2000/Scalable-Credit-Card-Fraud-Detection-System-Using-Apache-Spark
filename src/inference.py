913ee815e71210349048b1bb45ff84743a9b2baa
tejosumanth <pavuluritejo@gmail.com>
2024-06-02 20:09:00 +0000

    Add helper methods to inference.py

--- src/inference.py
+++ src/inference.py
@@ -34,3 +34,7 @@ def preprocess(object_key):
     numericCols = ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10',
        'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20',
        'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'Amount']
+    assembler = VectorAssembler(inputCols=numericCols, outputCol="features")
+    spark_df = assembler.transform(spark_df).select('features', 'Class', 'Amount') 
+
+    # Standardize input feature vector
