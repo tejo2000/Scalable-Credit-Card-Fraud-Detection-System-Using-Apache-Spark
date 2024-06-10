562e43625be735659830143215231b85339b0cca
tejosumanth <pavuluritejo@gmail.com>
2024-06-10 23:29:00 +0000

    Add configuration and setup code to train.py

--- src/train.py
+++ src/train.py
@@ -34,3 +34,7 @@ def train_model(df):
 
 def main():
     spark = get_spark()
+    # print(os.path.join(os.environ["HDFS_FILE_PATH"],sys.argv[1]))
+    preprocessed_df = spark.read.parquet(os.path.join(os.environ["HDFS_FILE_PATH"],sys.argv[1]))
+    train_model(preprocessed_df)
+
