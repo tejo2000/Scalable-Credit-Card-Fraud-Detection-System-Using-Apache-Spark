1df2e423967fb38a9f627a173420f3c35f972b98
tejosumanth <pavuluritejo@gmail.com>
2025-10-11 21:42:00 +0000

    Add requirements.txt with pyspark, boto3, python-dotenv, pandas

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,4 @@
 BUCKET_NAME = "fraud-detection-project"
 TEMP_MODEL_PATH = "model"
 PREDICTION_TABLE="fraud_prediction"
-# feat: add dotenv support for S3 bucket, HDFS path, and DB config
+# chore: add requirements.txt with pyspark, boto3, python-dotenv, pandas
